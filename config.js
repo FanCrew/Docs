285558024f3f0b527e2d13bef7dc0d8707088b64
DEF4ULT-OP <DEF4ULT-OP@users.noreply.github.com>
2020-12-07 09:03:49 +0000

    Site build: 20451472cff401d7bd7218f665a7c8a0ec60c860

--- config.js
+++ config.js
@@ -1,9 +1,13 @@
 module.exports = {
   openapi: '3.0.0',
-  host: 'https://fantasy.crustplay.com',
+  host: 'https://docs.crustplay.com',
   info: {
     title: 'FanCrew - API',
     version: '1.0.0'
   },
+  servers: [{
+    url: 'https://fantasy.crustplay.com',
+    description: 'Development Server'
+  }],
   apis: ['Routes/*.js', 'Models/*.js', 'Controllers/*.js', 'Services/*.js']
 };
