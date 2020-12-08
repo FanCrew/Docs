9dd201e3f71fb90da1a9541157d0500d79ae8aa1
DEF4ULT-OP <DEF4ULT-OP@users.noreply.github.com>
2020-12-08 17:00:51 +0000

    Site build: b8da3b1d1313db5cfbfa7fa83d9d6e23776670ea

---
+++ errors.js
@@ -0,0 +1,62 @@
+/**
+ * @swagger
+ * components:
+ *   responses:
+ *     400:
+ *       description: Bad Request
+ *       content: 
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               message:
+ *                 type: string
+ *     401: 
+ *       description: Authorization is required
+ *       content: 
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               message:
+ *                 type: string
+ *     403:
+ *       description: Unauthorised
+ *       content: 
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               message:
+ *                 type: string
+ *     404:
+ *       description: Not Found
+ *       content: 
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               message:
+ *                 type: string
+ *     422:
+ *       description: ValidationError
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               message: 
+ *                 type: string
+ *               errors:
+ *                  type: object
+ * 
+ *     500:
+ *       description: Internal Server Error
+ *       content: 
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               message:
+ *                 type: string
+ */
