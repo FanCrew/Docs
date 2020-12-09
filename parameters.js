72ff216bf7c25f02ade711d78874733a691b1b63
DEF4ULT-OP <DEF4ULT-OP@users.noreply.github.com>
2020-12-09 12:44:39 +0000

    Site build: 1f897916179718f46ecdb20215f7a40faa0c7323

---
+++ parameters.js
@@ -0,0 +1,21 @@
+/**
+ * @swagger
+ * components:
+ *   parameters:
+ *     match:
+ *       in: path
+ *       name: matchId
+ *       required: true
+ *       description: The match id
+ *       schema:
+ *         type: string
+ *         default: 5fba03763917ea4637e4fe6e
+ *     team:
+ *       in: path
+ *       name: teamId
+ *       required: true
+ *       description: The team id
+ *       schema:
+ *         type: string
+ *         default: 5fba68b3196b4d615e700cec
+ */
