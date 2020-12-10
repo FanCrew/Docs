83316151e4858f8367b4a7248795000dbbe25afb
DEF4ULT-OP <DEF4ULT-OP@users.noreply.github.com>
2020-12-10 04:06:45 +0000

    Site build: 90fbc43c4479424cbeccb88bc4c2e1a455be72d1

--- parameters.js
+++ parameters.js
@@ -2,6 +2,7 @@
  * @swagger
  * components:
  *   parameters:
+ * 
  *     match:
  *       in: path
  *       name: matchId
@@ -10,6 +11,7 @@
  *       schema:
  *         type: string
  *         default: 5fba03763917ea4637e4fe6e
+ * 
  *     team:
  *       in: path
  *       name: teamId
@@ -18,4 +20,21 @@
  *       schema:
  *         type: string
  *         default: 5fba68b3196b4d615e700cec
+ *     contest:
+ *       in: path
+ *       name: contestId
+ *       required: true
+ *       description: The contest id
+ *       schema:
+ *         type: string
+ *         default: 5fba0cdd23df2448b8bdf369
+ * 
+ *     player:
+ *       in: path
+ *       name: playerId
+ *       required: true
+ *       description: The player id
+ *       schema:
+ *         type: string
+ *         default: 5fba05f0f6bac046d456914e
  */
